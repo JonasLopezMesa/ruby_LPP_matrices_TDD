60516da2a86a116b9e4fd503830944a5ac8e900f
Jonás López Mesa <77jonas77@gmail.com>
2013-11-08 13:41:33 +0000

    Pruebas de operaciones básicas

--- spec/matrix_spec.rb
+++ spec/matrix_spec.rb
@@ -12,5 +12,32 @@ Module Math
             	expect(@p[2,1]).to eq(6)
             end
         end
+        #Debe poder realizar las siguientes operaciones básicas:
+		context "2. Deben poderse hacer las siguientes operaciones básicas:" do
+			it "2.1 Multiplicación" do
+				a = Matrix.new(3, 3)
+				b = a * @p
+				expect(b[1,1]).to be_a_kind_of(Fixnum)
+            	expect(b[1,1]).to eq(81)
+			end
+			it "2.2 División" do
+				a = Matrix.new(3, 3)
+				a[1,2] = 15
+				b = @p/a
+            	expect(b[1,1]).to eq(52.5)
+			end
+			it "2.3 Suma" do
+				a = Matrix.new(3, 3)
+				b = []
+				b = a + @p
+            	expect(b[4]).to eq(10)
+			end
+			it "2.4 Resta" do
+				a = Matrix.new(3, 3)
+				b = []
+				b = a - @p
+            	expect(b[4]).to eq(0)
+			end
+		end 
 	end
 end
