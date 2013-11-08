eb8f83eeadc4def468b529060eb65ba9c13c17bf
Sergio Miguel <alu0100706468@ull.edu.es>
2013-11-08 13:38:47 +0000

    Código Indexación

--- lib/Matrix.rb
+++ lib/Matrix.rb
@@ -1,5 +1,26 @@
 Module Math
 	class Matrix
-		
+		attr_accessor :m
+		attr_accessor :f
+		attr_accessor :c
+
+		def initialize(filas, columnas)
+			@f = filas
+			@c = columnas
+			@m = []
+			i = 0
+			while i < @f*@c
+				@m[i] = i+1
+				i=i+1
+			end
+		end
+
+		def [](a,b)
+			@m[a+(b*@c)]
+		end
+
+		def []=(a,b,r)
+			@m[a+(b*@c)] = r
+		end
 	end
 end
